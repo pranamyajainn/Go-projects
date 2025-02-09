aeb32dc279e30c23d43e026c7c62f7d2d8279595
Pranamya Jain <146780116+pranamyajainn@users.noreply.github.com>
2025-02-09 20:39:34 +0530

    Create print.go

---
+++ Basics/print.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+func main () {
+	age := 25
+	name := "John"
+	height := 1.75
+
+	fmt.Println(" age is ", age, "name is ", name , "height is ", height) 
+
+	fmt.Printf("age is %d", age)
+	fmt.Printf("name is %s\n", name);
+	fmt.Printf("Type of height is %T\n", height)
+}
